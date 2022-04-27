5721c0b327a91e7f0eb7f23345989e58c370a283
Julia Novitch <julia.novitch@gmail.com>
2022-04-27 23:25:48 +0300

    fix api root url

--- src/api/wp.js
+++ src/api/wp.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 
-const API_ROOT = 'http://cms.aurarosenberg.com/wp-json/wp/v2/'
+/* upgrading to v3 does not work, slug is lost */
+// const API_ROOT = 'http://cms.aurarosenberg.com/wp-json/acf/v3/'
+const API_ROOT = 'https://cms.aurarosenberg.com/wp-json/wp/v2/'
 const INFO_PAGE_ID = 6
 
 Vue.use(VueResource)
@@ -11,7 +13,9 @@ Vue.http.options.crossOrigin = true
 export default {
   getPosts () {
     return new Promise((resolve, reject) => {
-      Vue.http.get(API_ROOT + 'posts?per_page=100').then(response => {
+      Vue.http.get(API_ROOT + 'posts/?per_page=100').then(response => {
+      // Vue.http.get(API_ROOT + 'posts?per_page=100').then(response => {
+        console.log(response.body)
         resolve(response.body)
       }, (response) => {
         reject()
@@ -20,7 +24,7 @@ export default {
   },
   getInfo () {
     return new Promise((resolve, reject) => {
-      Vue.http.get(API_ROOT + 'pages?' + INFO_PAGE_ID).then(response => {
+      Vue.http.get(API_ROOT + 'pages/?' + INFO_PAGE_ID).then(response => {
         resolve(response.body)
       }, (response) => {
         reject()
